64a4dc69b270b7fd56ddd855ae3371b3f3a665c6
batnthn <batnthn@yahoo.com>
2024-09-09 23:14:59 -0400

    before i mess it up

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 def main():
-        with open("books/frankenstein.txt") as f:
-            file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    count = get_word_count(book_path)
+    print(text)
+    print(count)
+
+    
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
+def get_word_count(wordcount):
+    count = 0 
+    with open(wordcount) as f:
+        data = f.read()
+        lines = data.split()
+        count += len(lines)
+    return count
+
+def get_char_appearances(chars):
+    
+
+
+    
 main ()
+
