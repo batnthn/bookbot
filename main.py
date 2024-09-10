172116efff6610657fb921f3b103474a29471290
batnthn <batnthn@yahoo.com>
2024-09-09 23:21:40 -0400

    has book retrieval, word count, and character count

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count = get_word_count(book_path)
+    characters = get_char_appearances(book_path)
     print(text)
     print(count)
+    print(characters)
 
     
 def get_book_text(path):
@@ -20,7 +22,12 @@ def get_word_count(wordcount):
     return count
 
 def get_char_appearances(chars):
-    
+    char_count = dict()
+    with open(chars) as f:
+        data = f.read()
+        for da in data:
+            char_count[da] = char_count.setdefault(da, 0)+1
+    return char_count
 
 
     
